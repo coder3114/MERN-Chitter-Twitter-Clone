f6a869edb3c6545ef5390781a2c3980fe49de53d
coderella <qiangie.yang@gmail.com>
2023-08-03 17:42:45 +0100

    Project setup - backend server and mongodb connection

---
+++ server/server.js
@@ -0,0 +1,31 @@
+import cors from "cors";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import HTTPS from "https";
+
+dotenv.config({ path: ".env" });
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+// Mongoose setup
+const main = async () => {
+  console.log(`Connecting to: ${process.env.DB_URI}`);
+  await mongoose.connect(process.env.DB_URI);
+};
+
+main()
+  .then(`Connected to database`)
+  .catch((error) => console.log(`${error} did not connect`));
+
+const server = HTTPS.createServer(app).listen(process.env.PORT, () => {
+  const SERVERHOST = server.address().address;
+  const SERVERPORT = server.address().port;
+  console.log(`Server is running on https://${SERVERHOST}:${SERVERPORT}`);
+});
+
+export default server;
