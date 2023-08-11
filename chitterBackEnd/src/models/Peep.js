5f5b6f8f8f24bda645e25432b9ac881f935af099
coderella <qiangie.yang@gmail.com>
2023-08-11 17:04:10 +0100

    Design Peep Schema

--- chitterBackEnd/src/models/Peep.js
+++ chitterBackEnd/src/models/Peep.js
@@ -1,7 +1,21 @@
 import mongoose from "mongoose";
 
 const peepSchema = mongoose.Schema({
-  user_id: {},
-  description: {},
-  post_time: {},
+  user_id: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+    maxlength: 280,
+  },
+  post_time: {
+    type: Date,
+    default: Date.now,
+  },
 });
+
+const Peep = mongoose.model("Peep", peepSchema);
+export default Peep;
