c02533ff5eb7d98f260653077d4b80b6cba3b55a
coderella <qiangie.yang@gmail.com>
2023-08-20 18:46:15 +0100

    Add login user API tests

--- chitterFrontEnd/tests/userAPI.test.js
+++ chitterFrontEnd/tests/userAPI.test.js
@@ -1,6 +1,8 @@
 import axiosMock from "axios";
 import { describe, expect, beforeEach, test } from "vitest";
 
+import * as api from "../src/api/userAPI.js";
+
 import sampleUsers from "./testData/users.json";
 
 vi.mock("axios");
@@ -56,6 +58,66 @@ describe("External Data Tests", () => {
 
         axiosMock.post.mockRejectedValueOnce(expectedResponse);
         functionResult = await api.registerUser(testNewUser);
+
+        expect(functionResult).toStrictEqual(expectedReturn);
+      });
+    });
+  });
+
+  describe("loginUser tests", () => {
+    const userToLogin = { email: "test@domain.com", password: "password123" };
+    const sampleUserId = "0fje84h3nf";
+
+    describe("Base Request", () => {
+      beforeEach(() => {
+        api.loginUser(userToLogin);
+      });
+
+      test("should have made a post request to axios", () => {
+        expect(axiosMock.post).toHaveBeenCalled();
+      });
+
+      test('should make a request to the "/login" url with the user to login', () => {
+        expect(axiosMock.post).toHaveBeenCalledWith(
+          `${import.meta.env.VITE_PEEPSURL}/login`,
+          userToLogin
+        );
+      });
+    });
+
+    describe("Successful POST requests", () => {
+      test("should return the logged in user info with 201 status on success", async () => {
+        const status = 201;
+        const message = "some example text";
+        const data = { userId: sampleUserId, message: message };
+        const expectedResponse = { data: data, status: status };
+
+        const expectedReturn = {
+          userId: sampleUserId,
+          message: message,
+          status: status,
+        };
+
+        axiosMock.post.mockResolvedValueOnce(expectedResponse);
+        functionResult = await api.loginUser(userToLogin);
+
+        expect(functionResult).toStrictEqual(expectedReturn);
+      });
+    });
+
+    describe("Unsuccessful POST requests", () => {
+      test("should return an error property in the response when unsuccessful POST is made", async () => {
+        const status = 400;
+        const expectedResponse = {
+          response: { status: status, message: testError.message },
+        };
+        const expectedReturn = {
+          status: status,
+          error: { type: `post`, message: testError.message },
+        };
+
+        axiosMock.post.mockRejectedValueOnce(expectedResponse);
+        functionResult = await api.loginUser(userToLogin);
 
         expect(functionResult).toStrictEqual(expectedReturn);
       });
