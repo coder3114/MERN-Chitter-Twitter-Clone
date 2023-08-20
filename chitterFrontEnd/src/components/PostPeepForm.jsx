5138308b4eaf47bb1ea7afc7c97465fe447863d8
coderella <qiangie.yang@gmail.com>
2023-08-20 21:11:51 +0100

    Implement user isLoggedIn check and display a message

--- chitterFrontEnd/src/components/PostPeepForm.jsx
+++ chitterFrontEnd/src/components/PostPeepForm.jsx
@@ -2,7 +2,8 @@ import { useState } from "react";
 import PropTypes from "prop-types";
 import TimePosted from "./utils/TimePosted.jsx";
 
-const PostPeepForm = ({ submitAction }) => {
+const PostPeepForm = ({ submitAction, userId }) => {
+  const isLoggedIn = userId.length > 0;
   const [peepText, setPeepText] = useState("");
   const [peepTimePosted, setPeepTimePosted] = useState(null);
 
@@ -13,45 +14,53 @@ const PostPeepForm = ({ submitAction }) => {
     setPeepTimePosted(null);
   };
 
-  return (
-    <div className="container mt-5 mb-4">
-      <div className="row justify-content-center ">
-        <div className="card col-md-6">
-          <div className="card-body">
-            <form onSubmit={handlePeepSubmit}>
-              <div className="post-peep-form ">
-                <input
-                  className="form-control"
-                  type="text"
-                  name="peepText"
-                  value={peepText}
-                  placeholder="What's happening..."
-                  onChange={(event) => setPeepText(event.target.value)}
-                />
-              </div>
-              <div
-                className="card-text mt-2 mb-2 text-muted"
-                style={{ fontSize: 12 }}>
-                <label title="peepTimePosted">
-                  <TimePosted
-                    updateDateCreated={(dateCreated) =>
-                      setPeepTimePosted(dateCreated)
-                    }
+  if (isLoggedIn) {
+    return (
+      <div className="container mt-5 mb-4">
+        <div className="row justify-content-center ">
+          <div className="card col-md-6">
+            <div className="card-body">
+              <form onSubmit={handlePeepSubmit}>
+                <div className="post-peep-form ">
+                  <input
+                    className="form-control"
+                    type="text"
+                    name="peepText"
+                    value={peepText}
+                    placeholder="What's happening..."
+                    onChange={(event) => setPeepText(event.target.value)}
                   />
-                </label>
-                <input
-                  className="btn btn-primary float-right"
-                  type="submit"
-                  value="Post"
-                  disabled={!peepText}
-                />
-              </div>
-            </form>
+                </div>
+                <div
+                  className="card-text mt-2 mb-2 text-muted"
+                  style={{ fontSize: 12 }}>
+                  <label title="peepTimePosted">
+                    <TimePosted
+                      updateDateCreated={(dateCreated) =>
+                        setPeepTimePosted(dateCreated)
+                      }
+                    />
+                  </label>
+                  <input
+                    className="btn btn-primary float-right"
+                    type="submit"
+                    value="Post"
+                    disabled={!peepText}
+                  />
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="alert alert-primary" role="alert">
+        Logged in users can post peeps!
+      </div>
+    );
+  }
 };
 
 PostPeepForm.propTypes = {
