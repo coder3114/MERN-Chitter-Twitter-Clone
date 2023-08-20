5d5e24e9a135cf078530714aeb0b513cb5e79b26
coderella <qiangie.yang@gmail.com>
2023-08-20 21:07:45 +0100

    Add PostPeepForm tests

---
+++ chitterFrontEnd/tests/PostPeepForm.test.jsx
@@ -0,0 +1,41 @@
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import { describe, expect, test } from "vitest";
+import { fireEvent } from "@testing-library/react";
+
+import PostPeepForm from "../src/Components/PostPeepForm.jsx";
+
+describe(`Peep Component`, () => {
+  test(`it should render the component with valid peep data`, () => {
+    const submitAction = vi.fn();
+    const userId = "user123";
+    render(
+      <MemoryRouter>
+        <PostPeepForm submitAction={submitAction} userId={userId} />
+      </MemoryRouter>
+    );
+
+    expect(
+      screen.getByPlaceholderText("What's happening...")
+    ).toBeInTheDocument();
+    expect(screen.getByText("Post")).toBeInTheDocument();
+  });
+
+  test(" should disables the submit button when the peep text is empty", () => {
+    const submitAction = vi.fn();
+    const userId = "user123";
+
+    const { getByText, getByPlaceholderText } = render(
+      <PostPeepForm submitAction={submitAction} userId={userId} />
+    );
+
+    const input = getByPlaceholderText("What's happening...");
+    const submitButton = getByText("Post");
+
+    expect(submitButton).toBeDisabled();
+
+    fireEvent.change(input, { target: { value: "Test peep text" } });
+
+    expect(submitButton).not.toBeDisabled();
+  });
+});
