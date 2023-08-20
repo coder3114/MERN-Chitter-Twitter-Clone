395e263359754b36b736ebc083741557c247e366
coderella <qiangie.yang@gmail.com>
2023-08-20 13:54:55 +0100

    Implement existing user check to pass all tests

--- chitterBackEnd/src/controllers/user.controller.js
+++ chitterBackEnd/src/controllers/user.controller.js
@@ -1,4 +1,6 @@
 import { validationResult } from "express-validator";
+import { registerUserService } from "../services/users.service.js";
+import UserModel from "../models/UserModel.js";
 
 export const registerUser = async (req, res) => {
   const errors = validationResult(req);
@@ -6,6 +8,23 @@ export const registerUser = async (req, res) => {
     return res
       .status(422)
       .json({ message: `Validation failed, please check input!` });
+  }
+
+  const { username, email } = req.body;
+
+  // check Duplicate Username Or Email
+  const existingUserWithUsername = await UserModel.findOne({
+    username: username,
+  });
+  if (existingUserWithUsername) {
+    return res.status(400).json({ message: `Username is already in use!` });
+  }
+
+  const existingUserWithEmail = await UserModel.findOne({
+    email: email,
+  });
+  if (existingUserWithEmail) {
+    return res.status(400).json({ message: `Email is already in use!` });
   }
 
   try {
